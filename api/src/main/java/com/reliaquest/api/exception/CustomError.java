39c8613a4faa1d2a67acd9cdf203c8b22644e5ee
Shivam Sapate <shivamsapate@gmail.com>
2025-04-05 15:47:02 +0530

    Rearrange custom error messages

--- api/src/main/java/com/reliaquest/api/exception/CustomError.java
+++ api/src/main/java/com/reliaquest/api/exception/CustomError.java
@@ -4,24 +4,19 @@ import lombok.Getter;
 
 @Getter
 public enum CustomError {
-    REST_API_CALL_FAILURE("E1", "Failed to make the REST API request"),
-    JSON_PARSE_ERROR("E2", "Unable to parse the JSON response"),
+    NULL_EMPLOYEE_ID("E1", "Employee ID cannot be null"),
+    EMPLOYEE_NOT_FOUNT_BY_ID("E2", "Employee not found with the specified ID"),
     NO_EMPLOYEES_FOUND("E3", "No data available for employees"),
-    EMPLOYEE_NOT_FOUND_BY_NAME("E4", "Employee not found with the specified name"),
-    MISSING_OR_INVALID_NAME("E5", "Name is required"),
-//    INVALID_FIELD_TYPE_FOR_NAME("E6","Name must be a string"),
-//    INVALID_FIELD_TYPE_FOR_AGE("E6","Age must be an integer"),
-//    INVALID_FIELD_TYPE_FOR_SALARY("E6","Salary must be an integer"),
-//    INVALID_FIELD_TYPE_FOR_TITLE("E6","Title must be a string"),
+    MISSING_OR_INVALID_NAME("E4", "Name is required"),
+    EMPLOYEE_NOT_FOUND_BY_NAME("E5", "Employee not found with the specified name"),
     MISSING_OR_INVALID_SALARY("E6", "Salary is required"),
     NEGATIVE_SALARY_NOT_ALLOWED("E7", "Salary cannot be a negative value"),
-    MISSING_OR_INVALID_AGE("E8", "Age is required"),
-    NEGATIVE_AGE_NOT_ALLOWED("E9", "Age cannot be negative"),
-    INVALID_AGE_LIMIT("E10", "Age must be greater than 15 and less than 76"),
-    MISSING_TITLE("E11", "Title is required"),
-    NULL_EMPLOYEE_ID("E12", "Employee ID cannot be null"),
-    EMPLOYEE_NOT_FOUNT_BY_ID("E13", "Employee not found with the specified ID"),
-    EMPLOYEE_API_RATE_LIMIT_EXCEEDED("E14", "Employee api rate limit exceeded");
+    MISSING_TITLE("E8", "Title is required"),
+    MISSING_OR_INVALID_AGE("E9", "Age is required"),
+    NEGATIVE_AGE_NOT_ALLOWED("E10", "Age cannot be negative"),
+    INVALID_AGE_LIMIT("E11", "Age must be greater than 15 and less than 76"),
+    REST_API_CALL_FAILURE("E12", "Failed to make the REST API request"),
+    EMPLOYEE_API_RATE_LIMIT_EXCEEDED("E13", "Employee api rate limit exceeded");
 
 
 
